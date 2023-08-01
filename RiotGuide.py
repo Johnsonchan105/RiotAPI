0431355e68c6915b3cd1e4ae83ac4e84449aa046
Johnson Chan <44689240+Johnsonchan105@users.noreply.github.com>
2023-07-31 19:51:20 -0700

    added new stuff

--- RiotGuide.py
+++ RiotGuide.py
@@ -27,18 +27,38 @@ tft_match_history_api = "https://americas.api.riotgames.com/tft/match/v1/matches
 #concat match from match history
 tft_match_api = "https://americas.api.riotgames.com/tft/match/v1/matches/"
 
-api_key = "RGAPI-4dac3abd-984a-4a36-b864-696c2ca164e8"
+api_key = "RGAPI-1d1bf699-7b40-418c-bd97-97e75780522c"
 
 summoner_api_url = concat(tft_summoner_api, "IAMABEAN", api_key)
 
 resp = requests.get(summoner_api_url)
 
 playerInfo = resp.json()
-player_accountID = playerInfo["accountId"]
+#player_accountID = playerInfo["accountId"]
 puuid = playerInfo["puuid"]
 
 api_url_matches = match_api(tft_match_history_api, puuid, api_key)
+
 match_api(tft_match_history_api, puuid, api_key)
 player_matches = requests.get(api_url_matches)
+
+#list of matches
 matches = player_matches.json()
-print(matches)
+first_match = matches[0]
+
+api_url_match = concat(tft_match_api, first_match, api_key)
+match_data = requests.get(api_url_match)
+match_info = match_data.json()
+
+metadata = match_info['metadata']
+
+participantsList = metadata['participants']
+
+summonerIndex = participantsList.index(puuid)
+
+participants_match_info  = match_info['info']['participants']
+
+summoner_match_info = participants_match_info[summonerIndex]
+print(summoner_match_info)
+
+
